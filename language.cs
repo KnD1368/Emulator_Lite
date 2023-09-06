a36f0c4f41bff541bf265e58025e1f7e010bd7c2
KnD <92218610+KnD1368@users.noreply.github.com>
2023-09-06 18:43:46 +0700

    C# WPF

--- language.cs
+++ language.cs
@@ -1,4 +1,112 @@
+using nothing;
+
+public class KnD
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+}
 using noting;
+
+public class KnD
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+}
+using nothing;
+
+public class KnD
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+}
+using noting;
+
+public class KnD
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+}
+using nothing;
 
 public class KnD
 {
