fcba241748121f092f479b8ac204bbb207d37990
KnD <92218610+KnD1368@users.noreply.github.com>
2023-09-06 18:42:59 +0700

    C#

--- language.cs
+++ language.cs
@@ -25,3 +25,30 @@ public class KnD
         Console.WriteLine ("Python and C#");
     }
 }
+using noting;
+
+public class KnD
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+        Console.WriteLine ("Python and C#");
+    }
+}
